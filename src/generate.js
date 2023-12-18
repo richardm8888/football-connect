e6eba2bd096a907e5159d549e012a786407905a8
Richard  Moss <richard.moss@thestudentroom.com>
2023-12-18 10:50:08 +0000

    Load info from db and cache

--- src/generate.js
+++ src/generate.js
@@ -1,67 +1,93 @@
 const neo4j = require('neo4j-driver');
+import { sql } from "@vercel/postgres";
 
-const URI = 'neo4j://localhost:7687';
-const USER = 'neo4j';
-const PASSWORD = 'qwerty123';
+const URI = process.env.NEO4J_HOST || 'neo4j://localhost:7687';
+const USER = process.env.NEO4J_USER || 'neo4j';
+const PASSWORD = process.env.NEO4J_PASSWORD || 'qwerty123';
 const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
 
+async function getCached() {
+    const { rows } = await sql`SELECT * FROM games WHERE date = CURRENT_DATE;`;
+
+    return rows[0];
+}
+
+async function saveCached(gameData) {
+    await sql`INSERT INTO games (date, data) VALUES (CURRENT_DATE, ${JSON.stringify(gameData)});`;
+}
+
 async function getGameData() {
-    let finalPlayers = [];
-    let allSelectedPlayers = [];
-    try {
-        const { records, summary, keys } = await driver.executeQuery(
-            `MATCH (p:Player)-[:PLAYED_FOR]-(c:Club)-[:PLAYS_IN]-(cp:Competition WHERE cp.competitionId IN ['GB1'])
-            WITH p.playerId as playerId, p.name as playerName, c.clubId as clubId, c.name as clubName, count(*) as n_clubs WHERE n_clubs > 1
-            WITH playerId, playerName, clubId, clubName, n_clubs, rand() as r ORDER BY r LIMIT 1
-            RETURN playerId, playerName, clubId, clubName`,
-            {},
-            { database: 'football' }
-        );
 
-        for(record of records) {
-            // Add first player
-            finalPlayers[record.get('clubName').valueOf()] = [record.get('playerName')];
-            const players = await lookupPlayersByClub(record.get('clubId').valueOf(), record.get('playerId').valueOf(), 3, allSelectedPlayers);
+    const cached = await getCached();
 
-            for(player of players) {
-                finalPlayers[record.get('clubName').valueOf()].push(player.get('p').properties.name);
-                
-                finalPlayers[player.get('c2').properties.name.valueOf()] = [];
-                const morePlayers = await lookupPlayersByClub(player.get('c2').properties.clubId.valueOf(), player.get('p').properties.playerId.valueOf(), 4, allSelectedPlayers);
-                for(morePlayer of morePlayers) {
-                    finalPlayers[player.get('c2').properties.name.valueOf()].push(morePlayer.get('p').properties.name);
+    if (cached) {
+        return JSON.parse(cached.data);
+    } else {
+
+        let finalPlayers = [];
+        let allSelectedPlayers = [];
+        try {
+            const { records, summary, keys } = await driver.executeQuery(
+                `MATCH (p:Player)-[:PLAYED_FOR]-(c:Club)-[:PLAYS_IN]-(cp:Competition WHERE cp.competitionId IN ['GB1'])
+                WITH p.playerId as playerId, p.name as playerName, c.clubId as clubId, c.name as clubName, count(*) as n_clubs WHERE n_clubs > 1
+                WITH playerId, playerName, clubId, clubName, n_clubs, rand() as r ORDER BY r LIMIT 1
+                RETURN playerId, playerName, clubId, clubName`,
+                {},
+                { database: 'football' }
+            );
+
+            for(let record of records) {
+                // Add first player
+                finalPlayers[record.get('clubName').valueOf()] = [record.get('playerName')];
+                const players = await lookupPlayersByClub(record.get('clubId').valueOf(), record.get('playerId').valueOf(), 3, allSelectedPlayers);
+
+                for(let player of players) {
+                    finalPlayers[record.get('clubName').valueOf()].push(player.get('p').properties.name);
+                    
+                    finalPlayers[player.get('c2').properties.name.valueOf()] = [];
+                    const morePlayers = await lookupPlayersByClub(player.get('c2').properties.clubId.valueOf(), player.get('p').properties.playerId.valueOf(), 4, allSelectedPlayers);
+                    for(let morePlayer of morePlayers) {
+                        finalPlayers[player.get('c2').properties.name.valueOf()].push(morePlayer.get('p').properties.name);
+                    }
                 }
             }
+
+        } catch (error) {
+            console.error(error);
         }
 
-    } catch (error) {
-        console.error(error);
+
+        let i = 1;
+
+        let ret = []; 
+        for (const [key, value] of Object.entries(finalPlayers)) {
+            ret.push(
+                {
+                    category: key,
+                    words: value,
+                    difficulty: i
+                }
+            );
+            i++;
+        }
+
+        if (ret.length < 4) {
+            return getGameData();
+        }
+
+        saveCached(ret);
+
+        return ret;
     }
-
-
-    let i = 1;
-
-    let ret = []; 
-    for (const [key, value] of Object.entries(finalPlayers)) {
-        ret.push(
-            {
-                category: key,
-                words: value,
-                difficulty: i
-            }
-        );
-        i++;
-    }
-
-    console.log(ret);
-
-    return ret;
 }
 
 
 async function lookupPlayersByClub(clubId, playerId, numPlayers, excludePlayers) {
     const { records, summary, keys } = await driver.executeQuery(
-        `MATCH (p:Player)-[:PLAYED_FOR]-(c:Club {clubId: $clubId}) 
+        `
+        MATCH (p:Player)-[pf:PLAYED_FOR]-(:Club) 
+        WITH p, sum(pf.count) as totalPlayed WHERE totalPlayed > 50
+        MATCH (p)-[:PLAYED_FOR]-(c:Club {clubId: $clubId}) 
         WITH p, c WHERE NOT p.playerId IN $excludePlayers
         MATCH (p)-[:PLAYED_FOR]-(c2:Club)-[:PLAYS_IN]-(cp:Competition WHERE cp.competitionId IN ['GB1'])
         WITH p, c2, count(*) as n_clubs WHERE n_clubs > 1 AND c2.clubId <> $clubId
@@ -72,8 +98,5 @@ async function lookupPlayersByClub(clubId, playerId, numPlayers, excludePlayers)
 
     return records;
 }
-
-
-// getGameData(); //.then(data => console.log(data));
 
 export default getGameData;
--- src/providers/PuzzleDataProvider/PuzzleDataProvider.js
+++ src/providers/PuzzleDataProvider/PuzzleDataProvider.js
@@ -1,31 +1,42 @@
 import React, { useEffect } from "react";
-// import { puzzleAnswers } from "../../lib/time-utils";
-import getGameData from "../../generate";
+// import fetch from 'node-fetch';
 
 export const PuzzleDataContext = React.createContext();
 
 function PuzzleDataProvider({ children }) {
+    const [gameData, setGameData] = React.useState([]);
+
     useEffect(() => {
-    getGameData().then((data) => {
-        setGameData(data);   
-    });
-  }, []);
+        fetch("/api/game")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setGameData(result.GameData);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }, []);
 
-  const [gameData, setGameData] = React.useState([]);
-  const categorySize = gameData[0]?.words?.length ?? 0;
-  const numCategories = gameData?.length ?? 0;
 
-  if (!gameData.length) {
-    return <></>;
-  }
+    const categorySize = gameData[0]?.words?.length ?? 0;
+    const numCategories = gameData?.length ?? 0;
 
-  return (
-    <PuzzleDataContext.Provider
-      value={{ gameData, numCategories, categorySize }}
-    >
-      {children}
-    </PuzzleDataContext.Provider>
-  );
+    if (!gameData.length) {
+        return <></>;
+    }
+
+    return (
+        <PuzzleDataContext.Provider
+            value={{ gameData, numCategories, categorySize }}
+        >
+            {children}
+        </PuzzleDataContext.Provider>
+    );
 }
 
 export default PuzzleDataProvider;
