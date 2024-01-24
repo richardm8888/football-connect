cc1f110729d769c94f9e63b6d209639168779d71
richardm8888 <richy8888@hotmail.com>
2024-01-24 18:50:23 +0000

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -56,17 +56,6 @@ function App() {
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
-
-                  <script type="text/javascript">
-	atOptions = {
-		'key' : '24f3077a26ed67ffedec91719e64ee1d',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/24f3077a26ed67ffedec91719e64ee1d/invoke.js"></scr' + 'ipt>');
-</script>
                   
                 <Button
                     size="lg"
